4aa672ee8b5ea199a493dda602cc08cc20bb2e77
Firuz Khalimov <fkhalimov22@gmail.com>
2022-04-04 00:25:46 -0400

    Added ProjectController, ProjectDTO

---
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -0,0 +1,59 @@
+package com.cydeo.controller;
+
+
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.service.ProjectService;
+import com.cydeo.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/project")
+public class ProjectController {
+
+    ProjectService projectService;
+    UserService userService;
+
+    public ProjectController(ProjectService projectService, UserService userService) {
+        this.projectService = projectService;
+        this.userService = userService;
+    }
+
+    @GetMapping("/create")
+    public String createProject(Model model){
+        model.addAttribute("project",new ProjectDTO());
+        model.addAttribute("managers",userService.findManagers());
+        model.addAttribute("projects",projectService.findAll());
+
+        return "project/create";
+    }
+
+    @PostMapping("/create")
+    public String createProject2(ProjectDTO projectDTO){
+
+        projectService.save(projectDTO);
+
+        return "redirect:/project/create";
+    }
+
+    @GetMapping("/delete/{projectCode}")
+    public String deleteProject(@PathVariable("projectCode") String projectCode){
+
+        projectService.deleteById(projectCode);
+
+        return "redirect:/project/create";
+    }
+
+    @GetMapping("/complete/{projectCode}")
+    public String completeProject(@PathVariable("projectCode") String projectCode){
+
+        projectService.complete(projectService.findById(projectCode));
+
+        return "redirect:/project/create";
+    }
+
+
+
+
+}
---
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -0,0 +1,48 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.enums.Status;
+import com.cydeo.service.ProjectService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProjectServiceImpl extends AbstractMapService <ProjectDTO,String> implements ProjectService {
+    @Override
+    public ProjectDTO save(ProjectDTO object) {
+        if(object.getProjectStatus()==null){
+            object.setProjectStatus(Status.OPEN);
+        }
+        return super.save(object.getProjectCode(),object);
+    }
+
+    @Override
+    public List<ProjectDTO> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void update(ProjectDTO object) {
+
+         super.update(object.getProjectCode(),object);
+    }
+
+    @Override
+    public void deleteById(String code) {
+
+        super.deleteById(code);
+    }
+
+    @Override
+    public ProjectDTO findById(String id) {
+        return super.findById(id);
+    }
+
+
+    @Override
+    public void complete(ProjectDTO projectDTO) {
+        projectDTO.setProjectStatus(Status.COMPLETE);
+        super.save(projectDTO.getProjectCode(),projectDTO);
+    }
+}
